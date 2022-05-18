3ce0afb80d47d483110d907f7fda74cf454bb408
IntegralTeam <repo@sfxdx.com>
2022-05-18 09:48:56 +0300

    removed unused dep

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,6 @@ export { default as getRankInfo } from './getRankInfo';
 export * from './tronHelpers';
 export * from './api';
 export * from './getDateFormat';
-export { default as getUrlParameter } from './getUrlParameter';
 export { default as sleepForMs } from './sleepForMs';
 export * from './convertHelpers';
 export { default as getNetworkName } from './getNetworkName';
