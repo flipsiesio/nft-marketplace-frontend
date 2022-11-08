250834b6fcbd3ab0e5ffc514283da8d988eed66b
IntegralTeam <repo@sfxdx.com>
2022-11-08 06:46:16 +0300

    increaseGasLimit for mintRandom

--- src/store/nftMarket/saga/nftMarketMintNowSaga.ts
+++ src/store/nftMarket/saga/nftMarketMintNowSaga.ts
@@ -17,10 +17,26 @@ function* nftMarketMintNowSaga(
     const contract: Contract =
       yield getCardRandomMinterContract();
     const nftPrice: ethers.BigNumber = yield contract.getMintPrice(payload.token);
+
+    const increaseGasLimit = (estimatedGasLimit: ethers.BigNumber) => {
+      return estimatedGasLimit.mul(130).div(100); // increase by 30%
+    };
+
+    const gasLimit: ethers.BigNumber = yield contract.estimateGas.mintRandom(
+      payload.amount,
+      payload.token,
+      {
+        value: nftPrice.mul(payload.amount).toString(),
+      },
+    );
+
     const tx: ContractTransaction = yield contract.mintRandom(
       payload.amount,
       payload.token,
-      { value: nftPrice.mul(payload.amount).toString() },
+      {
+        value: nftPrice.mul(payload.amount).toString(),
+        gasLimit: increaseGasLimit(gasLimit),
+      },
     );
     yield tx.wait();
     yield put(apiActions.success(type));
